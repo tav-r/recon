7bf8de7dfd3233b32fa4ef2c01d589127d2a00ae
David Herrmann <david.herrmann@protonmail.com>
2021-11-12 19:11:56 +0100

    get common names script added

---
+++ get_common_names.py
@@ -0,0 +1,57 @@
+"""
+Get SSL common names on port 443 for addresses read from stdin.
+"""
+
+import ssl
+import json
+import functools
+
+import OpenSSL.crypto as crypto
+
+from typing import Iterator, Callable, List, Any
+
+
+def catch_wrapper(g: Callable, exceptions: List[Any]):
+    def _catch_wrapper(f):
+        @functools.wraps(f)
+        def _wrapped(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except exceptions:
+                g()
+
+        return _wrapped
+
+    return _catch_wrapper
+
+
+def iter_stdin() -> Iterator:
+    while True:
+        try:
+            yield(input())
+        except EOFError:
+            return
+
+
+@catch_wrapper(lambda: ..., [ConnectionRefusedError, ConnectionResetError])
+def get_cn(ip: str, port: int):
+    dst = (ip, 443)
+    cert = ssl.get_server_certificate(dst).encode()
+    x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+    cert_hostname = x509.get_subject().CN
+
+    for name in cert_hostname.split("\n"):
+        yield name
+
+
+def run_from_stdin():
+    print(json.dumps([
+        functools.reduce(
+            lambda a, b: a + b,
+            get_cn(line.strip(), 443)
+        ) for line in iter_stdin()
+    ]))
+
+
+if __name__ == "__main__":
+    run_from_stdin()
